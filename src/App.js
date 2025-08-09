384164eb3aab8c9dfc7e9e0c3aa90a9a1b22ee35
2300033132 <2300033132@kluniversity.in>
2025-08-09 10:58:02 +0530

    4th

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
-
-
+import logo from './logo.svg';
+import git1 from './git1.jpg';
 import './App.css';
 
 function App() {
   return (
-   <div>
-    <h1>GITHUB !!!!!</h1>
-   </div>
+    <div className="App">
+      <header className="App-header">
+        {/* React spinning logo */}
+        <img src={logo} className="App-logo" alt="logo" />
+
+        <p>Dhakshayani !!!</p>
+
+        <a
+          className="App-link"
+          href="https://learn.github.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn GITHUB through "GITHUB Learn" !!
+        </a>
+
+        {/* Cute picture in the corner */}
+        <img
+  src={git1}
+  alt="git logo"
+  style={{
+    position: 'absolute',
+    bottom: '20px',
+    right: '20px',
+    width: '180px',        // bigger size
+    height: 'auto',        // keep proportions
+    borderRadius: '15px',  // rounded corners
+    boxShadow: '0px 4px 8px rgba(0,0,0,0.3)' // subtle shadow
+  }}
+/>
+      </header>
+    </div>
   );
 }
 
